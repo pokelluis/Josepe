6cb2b37e4065c333e331db8f34c6f347de29b798
pokelluis <167977187+pokelluis@users.noreply.github.com>
2024-10-16 17:34:16 +0200

    Bateria de Ejercicios

--- CosmosKernel1/Kernel.cs
+++ CosmosKernel1/Kernel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
@@ -7,18 +7,62 @@ namespace CosmosKernel1
 {
     public class Kernel : Sys.Kernel
     {
+        private const string Value = "Bienvenido a Lluiso";
 
         protected override void BeforeRun()
         {
-            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            Console.Clear();
+            Console.WriteLine(Value);
         }
 
+        protected static void Menu(string input){
+            if (input == "help")
+            {
+                Help();
+            }
+            else if (input == "about")
+            {
+                About();
+            }else if (input == "shutdown")
+            {
+                Shutdown();
+            }else if (input == "restart"){
+                Reboot();
+            }
+            else
+            {
+                Console.WriteLine("Comando deconocido.");
+            }
+        }
+        protected static void Help()
+        {
+            Console.WriteLine("Con el comando de about podras ver de que va el sistema operativo");
+            Console.WriteLine("Con el comando de close podras cerrar el dichoso sistema opertaivo");
+            Console.WriteLine("Con el comando de restart podras reiniciar el sistema operativo");
+        }
+
+        protected static void About(){
+            Console.WriteLine("Este es un sistema operativo para autistas como el nombre indica");
+        }
+
+        static void Reboot()
+        {
+            Console.WriteLine("Reiniciant el sistema...");
+            Cosmos.System.Power.Reboot();
+        }
+
+        static void Shutdown()
+        {
+            Console.WriteLine("Apagant el sistema...");
+            Cosmos.System.Power.Shutdown();
+        }        
         protected override void Run()
         {
-            Console.Write("Input: ");
-            var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+            string input = "";
+            Console.WriteLine("Escribe \"help\" para recibir una guia de comandos.");
+            input = Console.ReadLine();
+            Menu(input);
         }
+
     }
 }
